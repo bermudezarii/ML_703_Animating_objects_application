924e0e07ef98d82e08c9e3a4c5044e70ff6aeae5
test <test@test.com>
2022-05-06 16:17:32 +0400

    Added the classifier

---
+++ CNN_Binary_Template_2.0/code/models.py
@@ -0,0 +1,43 @@
+"""
+    Definition of Models
+
+    * Implemented Models:
+        - ResNet 18
+        - EfficientNet B4
+"""
+
+from torch import nn
+from torchvision import models
+from hyperparameters import parameters as params
+
+
+def resnet18():
+    """
+        efficientnet ResNet 18 model definition.
+    """
+    out_features = params['out_features']
+
+    model = models.resnet18(pretrained=True)
+
+    # To freeze layers
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # New output layers
+    num_ftrs = model.fc.in_features
+    model.fc = nn.Linear(num_ftrs, out_features)
+
+    return model
+
+
+def efficientnet():
+    """
+        efficientnet EfficientNet B4 model definition.
+    """
+    out_features = params['out_features']
+
+    model = models.efficientnet_b4(pretrained=True)
+
+    model.classifier[1] = nn.Linear(in_features=model.classifier[1].in_features, out_features=out_features)
+
+    return model
